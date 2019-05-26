806267c8b57ce947604aff24c66679f3a8c660a6
Batuhan Taşkaya <btaskaya33@gmail.com>
2019-05-26 12:04:12 +0300

    better matching, readme, formatting.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Stallman's Bot
+Not <thing>, GNU/<thing> please. 
+
+[![Preview](https://i.hizliresim.com/dLjqED.png)](https://clips.twitch.tv/CovertColdbloodedReubenCoolCat)
+## Handlers
+Feel free to add handlers and PR.
+
+- nano
+- linux
+- emacs
+- grep
--- stallmansbot.py
+++ stallmansbot.py
@@ -1,20 +1,21 @@
-import socket
 import re
+import socket
 from collections import defaultdict
-from io import StringIO
 from contextlib import suppress
+from io import StringIO
+
 
 class Client:
     ADDR = ("irc.twitch.tv", 6667)
     _callbacks = defaultdict(list)
-    
+
     def __init__(self, nick, password):
         self.con = socket.socket()
         self.con.connect(self.ADDR)
 
         self.push_cmd("pass", password)
         self.push_cmd("nick", nick)
-        
+
         self.mode = "run"
 
     @classmethod
@@ -22,18 +23,19 @@ class Client:
         def wrapper(func):
             cls._callbacks[patterns].append(func)
             return func
+
         return wrapper
-    
+
     def connect(self, room):
         self._room = room
         self.push_cmd("join", room)
-        
+
         buffer = str()
         while self.mode != "quit":
-            buffer = buffer + self.con.recv(1024).decode('UTF-8')
+            buffer = buffer + self.con.recv(1024).decode("UTF-8")
             _buffer = re.split(r"[~\r\n]+", buffer)
             buffer = _buffer.pop()
-            
+
             for line in _buffer:
                 line = line.strip().split()
                 if len(line) >= 1:
@@ -41,9 +43,10 @@ class Client:
                         self.push_cmd("pong", line[1])
                     if line[1] == "PRIVMSG":
                         author, *message = line[2:]
-                        message = ' '.join(message)[1:]
+                        message = " ".join(message)[1:]
                         author = author[1:]
-                        
+
+                        print(f"{author}: {message}")
                         for patterns, callbacks in self._callbacks.items():
                             pass_this = True
                             matches = []
@@ -61,19 +64,21 @@ class Client:
 
     def push_cmd(self, cmd, value):
         request = f"{cmd.upper()} {value}\r\n"
-        self.con.send(request.encode('utf8'))
-    
+        self.con.send(request.encode("utf8"))
+
     def send_message(self, message):
         self.push_cmd("privmsg", f"{self._room} :{message}")
-    
+
+
 @Client.register("nano", "linux", "emacs", "grep")
 def gnu_receiver(self, author, message, matches):
     def not_generator(thing):
         return f"Not {thing}, GNU/{thing.title()}"
-    
-    if "gnu" not in message:
+
+    if "gnu" not in message.lower():
         msg = ". ".join(not_generator(thing) for thing in matches)
         self.send_message(f"Guys, please. {msg}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     pass
