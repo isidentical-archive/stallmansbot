ee9d85d962d35a656ecf3b3a22c5c1dcb30259cb
Batuhan Taşkaya <btaskaya33@gmail.com>
2019-05-26 13:10:16 +0300

    add channel switching for better trolling

--- stallmansbot.py
+++ stallmansbot.py
@@ -46,18 +46,23 @@ class Client:
         self.push_cmd("join", room)
 
         buffer = str()
-        while self.mode != "quit":
-            buffer = buffer + self.con.recv(1024).decode("UTF-8")
-            _buffer = re.split(r"[~\r\n]+", buffer)
-            buffer = _buffer.pop()
+        try:
+            while self.mode != "quit":
+                with suppress(OSError):
+                    buffer = buffer + self.con.recv(1024).decode("UTF-8")
+                    _buffer = re.split(r"[~\r\n]+", buffer)
+                    buffer = _buffer.pop()
 
-            for line in _buffer:
-                line = line.strip().split()
-                if len(line) >= 1:
-                    if line[0] == "PING":
-                        self.push_cmd("pong", line[1])
-                    if line[1] == "PRIVMSG":
-                        self.dispatch_message(line)
+                    for line in _buffer:
+                        line = line.strip().split()
+                        if len(line) >= 1:
+                            if line[0] == "PING":
+                                self.push_cmd("pong", line[1])
+                            if line[1] == "PRIVMSG":
+                                self.dispatch_message(line)
+        except KeyboardInterrupt:
+            new_channel_to_spread_free_software_movement = input("Channel: ")
+            self.connect(new_channel_to_spread_free_software_movement)
 
     def dispatch_message(self, line):
         author = self.obtain_author(line.pop(0))
@@ -82,7 +87,6 @@ class Client:
 
     def push_cmd(self, cmd, value):
         request = f"{cmd.upper()} {value}\r\n"
-        print(request)
         self.con.send(request.encode("utf8"))
 
     def send_message(self, room, message):
